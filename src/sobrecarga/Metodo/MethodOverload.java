1ba2a7553ad2c21939cf9235c7ef8ca30def21cd
DavidSamerson <samerson10@gmail.com>
2017-05-24 17:13:02 -0300

    sobrecarga de métodos

---
+++ src/sobrecarga/Metodo/MethodOverload.java
@@ -0,0 +1,24 @@
+package sobrecarga.Metodo;
+
+public class MethodOverload {
+	
+	public void testOverloadMethods(){
+		
+		System.out.printf("Square Integer 7 is %d\n",square(7));
+		System.out.printf("Square Double 7 is %f\n",square(7.5));
+		
+	}
+
+	private double square(double doubleValue) {
+		// TODO Auto-generated method stub
+		System.out.printf("\n convertendo inteiro %d",doubleValue);
+		return doubleValue * doubleValue;
+	}
+
+	private int square(int intValue) {
+		// TODO Auto-generated method stub
+		System.out.printf("\n convertendo inteiro %d",intValue);
+		return intValue * intValue;
+	}
+
+}
