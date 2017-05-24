07d75c037b737f60c6e52951eb53f9b3b550e3b6
DavidSamerson <samerson10@gmail.com>
2017-05-24 17:26:34 -0300

    modificações

--- src/sobrecarga/Metodo/MethodOverload.java
+++ src/sobrecarga/Metodo/MethodOverload.java
@@ -4,20 +4,20 @@ public class MethodOverload {
 	
 	public void testOverloadMethods(){
 		
-		System.out.printf("Square Integer 7 is %d\n",square(7));
-		System.out.printf("Square Double 7 is %f\n",square(7.5));
+		System.out.printf("\nSquare Integer 7 is %d\n",square(7));
+		System.out.printf("\nSquare Double 7 is %f\n",square(7.5));
 		
 	}
 
 	private double square(double doubleValue) {
 		// TODO Auto-generated method stub
-		System.out.printf("\n convertendo double %f",doubleValue);
+		System.out.printf("\nconvertendo double %f",doubleValue);
 		return doubleValue * doubleValue;
 	}
 
 	private int square(int intValue) {
 		// TODO Auto-generated method stub
-		System.out.printf("\n convertendo inteiro %d",intValue);
+		System.out.printf("\nconvertendo inteiro %d",intValue);
 		return intValue * intValue;
 	}
 
