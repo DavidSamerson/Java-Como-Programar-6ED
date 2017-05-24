4f8bf4c72f845521683e8d32da59fae1f6a0f279
DavidSamerson <samerson10@gmail.com>
2017-05-24 16:51:03 -0300

    escopo de variaveis

---
+++ src/questão/Escopo/ScopeTest.java
@@ -0,0 +1,12 @@
+package questão.Escopo;
+
+public class ScopeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scope executeScope = new Scope();
+		executeScope.begin();
+	}
+
+}
