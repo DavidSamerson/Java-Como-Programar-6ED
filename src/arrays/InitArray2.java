45df5677dc622306abcb7cfa70645cb7e832a2c1
DavidSamerson <samerson10@gmail.com>
2017-05-25 16:17:19 -0300

    arrays 2

---
+++ src/arrays/InitArray2.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class InitArray2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int array[] = {32,27,64,18,95,14,90,70,60,37};
+		
+		
+
+	}
+
+}
