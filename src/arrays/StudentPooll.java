3f14b36b8c5f6477ffbc2d7bb68c1e812757eed9
DavidSamerson <samerson10@gmail.com>
2017-05-26 20:47:10 -0300

    studentpool terminada

--- src/arrays/StudentPooll.java
+++ src/arrays/StudentPooll.java
@@ -5,12 +5,17 @@ public class StudentPooll {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int responses[] = {1,2,3,45,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
+		int responses[] = {1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
 		
 		int frequency[] = new int[11];
 		
+		for(int amswer = 0; amswer < responses.length; amswer++){
+			++frequency[responses[amswer]];}
 		
-
+		System.out.printf("%s%10s\n","Rating","Frequency");
+		
+		for(int rating = 1; rating < frequency.length; rating++){
+			System.out.printf("%6d%10d\n", rating, frequency[rating]);}
 	}
 
 }
