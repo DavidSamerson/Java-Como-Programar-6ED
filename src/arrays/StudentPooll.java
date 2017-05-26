0be78add9e7a0ab83a67ea750ccf124a0d617d04
DavidSamerson <samerson10@gmail.com>
2017-05-26 20:34:45 -0300

    contador de respostas

---
+++ src/arrays/StudentPooll.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class StudentPooll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int responses[] = {1,2,3,45,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
+		
+		int frequency[] = new int[11];
+		
+		
+
+	}
+
+}
