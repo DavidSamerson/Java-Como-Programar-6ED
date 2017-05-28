4786effafa936930c7bf820f3b15a4cfc4ad8f18
DavidSamerson <samerson10@gmail.com>
2017-05-28 19:56:12 -0300

    inicio da classe gradebook soma de notas

---
+++ src/utilizando/Array/Para/Armazenar/Notas/GradeBook.java
@@ -0,0 +1,23 @@
+package utilizando.Array.Para.Armazenar.Notas;
+
+public class GradeBook {
+
+	private String courseName;
+	private int grades[];
+
+	public GradeBook(String name, int gradesArray[]) 
+	{
+		courseName = name;
+		grades = gradesArray;
+	}
+	
+	public void setCourseName( String name )
+	{
+		courseName = name;
+	}
+	
+	public String getCourseName()
+	{
+		return courseName;
+	}
+}
