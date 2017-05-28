843f12da20c9a905aae597affe7f061dce9c5028
DavidSamerson <samerson10@gmail.com>
2017-05-28 15:01:24 -0300

    final classe

--- src/simulador/Embaralha/Distribui/Cartas/DeckOfCards.java
+++ src/simulador/Embaralha/Distribui/Cartas/DeckOfCards.java
@@ -11,19 +11,40 @@ public class DeckOfCards {
 	private final int NUMBER_OF_CARDS = 52;
 	private Random randomNumbers;
 
-	public DeckOfCards() 
-	{
-		String faces[] = {"ACE","DEUCE","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
-		String suits[] = {"HEARTHS","DIAMONDS","CLUBS","SPADES"};
-		
+	public DeckOfCards() {
+		String faces[] = { "ACE", "DEUCE", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK",
+				"QUEEN", "KING" };
+		String suits[] = { "HEARTHS", "DIAMONDS", "CLUBS", "SPADES" };
+
 		deck = new Card[NUMBER_OF_CARDS];
 		currentCard = 0;
 		randomNumbers = new Random();
-		
-		for( int count=0; count < deck.length; count++){
-			
-			deck[ count ] = new Card( faces[ count % 13 ], suits [ count / 13 ] ); 
+
+		for (int count = 0; count < deck.length; count++) {
+
+			deck[count] = new Card(faces[count % 13], suits[count / 13]);
 		}
+	}
+
+	public void shuffle() {
+		currentCard = 0;
+
+		for (int first = 0; first < deck.length; first++) {
+			int second = randomNumbers.nextInt(NUMBER_OF_CARDS);
+
+			Card temp = deck[first];
+			deck[first] = deck[second];
+			deck[second] = temp;
+
+		}
+	}
+
+	public Card dealCard() {
+
+		if (currentCard < deck.length)
+			return deck[currentCard++];
+		else
+			return null;
 	}
 
 }
