033ddb0d4a8c2feaa3b89c4f3f15acbffa50d87a
DavidSamerson <samerson10@gmail.com>
2017-05-29 23:13:25 -0300

    arraybidimencional

---
+++ src/array/Bidimencinal/Notas/GradeBook.java
@@ -0,0 +1,14 @@
+package array.Bidimencinal.Notas;
+
+public class GradeBook {
+	
+	private String courseName;
+	private int grades [][];
+	
+	
+	public GradeBook(String name, int gradesArray[][])
+	{
+		
+	}
+
+}
