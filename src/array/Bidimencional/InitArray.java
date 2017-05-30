510e8741fe334756e199eef64bfcf47b3f99b2e1
DavidSamerson <samerson10@gmail.com>
2017-05-29 22:48:12 -0300

    iniciando array bidimencional

---
+++ src/array/Bidimencional/InitArray.java
@@ -0,0 +1,18 @@
+package array.Bidimencional;
+
+public class InitArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int array1 [][] = {{1,2,3},{4,5,6}};
+		int array2 [][] = {{1,2},{3},{4,5,6}};
+		
+		System.out.println("Values in array1 by row are");
+		outputArray(array1);
+		
+		System.out.println("\nValues in array2 by row are");
+
+	}
+
+}
