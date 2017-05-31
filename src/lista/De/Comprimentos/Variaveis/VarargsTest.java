bbcba2d310ac7822f051ca425aad6bfb5741d958
DavidSamerson <samerson10@gmail.com>
2017-05-31 15:04:27 -0300

    comprimentos variaveis

---
+++ src/lista/De/Comprimentos/Variaveis/VarargsTest.java
@@ -0,0 +1,15 @@
+package lista.De.Comprimentos.Variaveis;
+
+public class VarargsTest {
+	
+	public static double average(double... numbers)
+	{
+		return 0.0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
