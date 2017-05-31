cdb88ef35ff4cd2fe63f398f5b6c74948fcf7377
DavidSamerson <samerson10@gmail.com>
2017-05-31 15:33:05 -0300

    Utilizando Argumentos de linha de comando

---
+++ src/argumentos/Linha/De/Comando/InitArray.java
@@ -0,0 +1,13 @@
+package argumentos.Linha.De.Comando;
+
+public class InitArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		if (args.length != 3)
+			System.out.println("ERRO. reiniciar programa!");
+
+	}
+
+}
