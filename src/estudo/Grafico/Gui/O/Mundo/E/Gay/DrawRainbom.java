f37f0f138f0950655c19c3b5e97fd2d7bf4e1e63
DavidSamerson <samerson10@gmail.com>
2017-05-31 17:06:46 -0300

    rainbom

--- src/estudo/Grafico/Gui/O/Mundo/E/Gay/DrawRainbom.java
+++ src/estudo/Grafico/Gui/O/Mundo/E/Gay/DrawRainbom.java
@@ -1,5 +1,18 @@
 package estudo.Grafico.Gui.O.Mundo.E.Gay;
 
-public class DrawRainbom {
+import java.awt.Color;
+import javax.swing.JPanel;
 
+public class DrawRainbom extends JPanel{
+
+	final Color VIOLET = new Color (128,0,128);
+	final Color INDIGO = new Color (75, 0, 130);
+	
+	private Color colors[] = {Color.WHITE, Color.WHITE, VIOLET, INDIGO, Color.BLUE,
+								Color.GREEN, Color.YELLOW, Color.ORANGE, Color.RED};
+	
+	public DrawRainbom()
+	{
+		
+	}
 }
