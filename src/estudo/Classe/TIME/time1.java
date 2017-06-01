bf1b0e3c1778c43195641591b244134621305fac
DavidSamerson <samerson10@gmail.com>
2017-06-01 14:10:24 -0300

    classe time terminada

--- src/estudo/Classe/TIME/time1.java
+++ src/estudo/Classe/TIME/time1.java
@@ -6,18 +6,20 @@ public class time1 {
 	private int minute;
 	private int seconds;
 
-	public void setTime(int h, int m, int s) 
-	{
-		
+	public void setTime(int h, int m, int s) {
+
 		hour = ((h >= 0 && h < 24) ? h : 0);
 		minute = ((m >= 0 && m < 60) ? m : 0);
 		seconds = ((s >= 0 && s < 60) ? s : 0);
 
 	}
-	
-	public String toUniversalString()
-	{
-		return String.format("%02d:%02d:%02d", hour,minute,seconds);
+
+	public String toUniversalString() {
+		return String.format("%02d:%02d:%02d", hour, minute, seconds);
+	}
+
+	public String toString() {
+		return String.format("%d:%02d:%02d %s", ((hour == 0 || hour == 12) ? 12 : hour % 12), minute, seconds, (hour < 12 ? "AM" : "PM"));
 	}
 
 }
