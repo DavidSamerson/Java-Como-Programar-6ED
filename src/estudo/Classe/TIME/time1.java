93946c5f1d557667762600e5404dedf049129db2
DavidSamerson <samerson10@gmail.com>
2017-06-01 13:47:05 -0300

    time

--- src/estudo/Classe/TIME/time1.java
+++ src/estudo/Classe/TIME/time1.java
@@ -1,5 +1,14 @@
 package estudo.Classe.TIME;
 
 public class time1 {
+	
+	private int hour;
+	private int minute;
+	private int seconds;
+	
+	public void setTime( int h, int m, int s )
+	{
+		
+	}
 
 }
