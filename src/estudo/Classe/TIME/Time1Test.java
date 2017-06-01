e9814668a5555964491cf0ad28c4bb8e418bc259
DavidSamerson <samerson10@gmail.com>
2017-06-01 15:02:07 -0300

    Classe Time Teste

---
+++ src/estudo/Classe/TIME/Time1Test.java
@@ -0,0 +1,20 @@
+package estudo.Classe.TIME;
+
+public class Time1Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		time1 time = new time1();
+		
+		System.out.print("The universal initial time is: ");
+		System.out.println(time.toUniversalString());
+		System.out.print("The initial standard time is: ");
+		System.out.println(time.toString());
+		System.out.println();
+		
+		
+
+	}
+
+}
