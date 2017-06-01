48f54d7665aa992d1aadfbd3be280ad854980223
DavidSamerson <samerson10@gmail.com>
2017-06-01 13:59:20 -0300

    time. retorno string

--- src/estudo/Classe/TIME/time1.java
+++ src/estudo/Classe/TIME/time1.java
@@ -6,10 +6,18 @@ public class time1 {
 	private int minute;
 	private int seconds;
 
-	public void setTime(int h, int m, int s) {
+	public void setTime(int h, int m, int s) 
+	{
+		
 		hour = ((h >= 0 && h < 24) ? h : 0);
 		minute = ((m >= 0 && m < 60) ? m : 0);
 		seconds = ((s >= 0 && s < 60) ? s : 0);
+
+	}
+	
+	public String toUniversalString()
+	{
+		return String.format("%02d:%02d:%02d", hour,minute,seconds);
 	}
 
 }
