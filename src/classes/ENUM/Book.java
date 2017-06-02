865f48b42e916e37b14595ab026e7f7a35ca5239
DavidSamerson <samerson10@gmail.com>
2017-06-02 19:19:28 -0300

    classe book

---
+++ src/classes/ENUM/Book.java
@@ -0,0 +1,16 @@
+package classes.ENUM;
+
+public enum Book {
+	
+	JHTP6("A","a"),
+	CHTP4("B","b"),
+	IW3HTP3("C","c"),
+	CPPHTP4("D","d"),
+	VBHTP2("E","e"),
+	CSHARPHTP("F","f");
+	
+	private final String title;
+	private final String yeah;
+	
+	
+}
