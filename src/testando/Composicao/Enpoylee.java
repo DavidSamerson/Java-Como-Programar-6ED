764e24adcf7ddcc70dce6936ef656034e92ef35a
DavidSamerson <samerson10@gmail.com>
2017-06-02 15:38:25 -0300

    classe Enpoilee terminada

--- src/testando/Composicao/Enpoylee.java
+++ src/testando/Composicao/Enpoylee.java
@@ -3,10 +3,20 @@ package testando.Composicao;
 public class Enpoylee {
 	
 	private String firstName;
-	private String LastName;
+	private String lastName;
 	private Date birthDate;
 	private Date hireDate;
 	
+	public Enpoylee (String first, String last, Date dateOfBirth, Date dateOfHide )
+	{
+		firstName = first;
+		lastName = last;
+		birthDate = dateOfBirth;
+		hireDate = dateOfHide;
+	}
 	
-
+	public String toString()
+	{
+		return String.format("%s, %s hired: %s ; BirthDAY: %s", lastName, firstName, hireDate, birthDate);	
+	}
 }
