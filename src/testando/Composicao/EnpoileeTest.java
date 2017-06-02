b15a25241c42d55b65c3a12f7349b2fe0b09a255
DavidSamerson <samerson10@gmail.com>
2017-06-02 17:02:26 -0300

    correção de datas

--- src/testando/Composicao/EnpoileeTest.java
+++ src/testando/Composicao/EnpoileeTest.java
@@ -5,8 +5,8 @@ public class EnpoileeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Date birth = new Date(24, 7, 1949);
-		Date hide = new Date(12, 3, 1988);
+		Date birth = new Date(7, 24, 1949);
+		Date hide = new Date(3, 12, 1988);
 
 		Enpoylee enpoilee = new Enpoylee("Bob", "Blue", birth, hide);
 		
