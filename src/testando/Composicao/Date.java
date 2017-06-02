ba9f7a8f117185b37de623667e28cff217b366d6
DavidSamerson <samerson10@gmail.com>
2017-06-02 13:30:21 -0300

    criação da classe date

---
+++ src/testando/Composicao/Date.java
@@ -0,0 +1,36 @@
+package testando.Composicao;
+
+public class Date {
+
+	private int month;
+	private int day;
+	private int yeah;
+
+	public Date(int theMonth, int theDay, int theYear) 
+	{
+		month = checkMonth(theMonth);
+		yeah = theYear;
+		day = checkDay(theDay);
+
+		System.out.printf("Date Object constructor for date %s\n", this);
+	}
+
+	private int checkMonth(int testMonth) 
+	{
+		// TODO Auto-generated method stub
+
+		if (testMonth > 0 && testMonth <= 12)
+			return testMonth;
+
+		else {
+			return testMonth;
+		}
+	}
+
+	private int checkDay(int theDay) 
+	3{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
