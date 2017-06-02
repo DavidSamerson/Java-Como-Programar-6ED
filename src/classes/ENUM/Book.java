da37f8de78263f324b69bf07516fdf915eb7ade4
DavidSamerson <samerson10@gmail.com>
2017-06-02 19:36:09 -0300

    correção

--- src/classes/ENUM/Book.java
+++ src/classes/ENUM/Book.java
@@ -1,30 +1,23 @@
 package classes.ENUM;
 
 public enum Book {
-	
-	JHTP6("A","a"),
-	CHTP4("B","b"),
-	IW3HTP3("C","c"),
-	CPPHTP4("D","d"),
-	VBHTP2("E","e"),
-	CSHARPHTP("F","f");
-	
+
+	JHTP6("A", "2000"), CHTP4("B", "2001"), IW3HTP3("C", "2002"), CPPHTP4("D", "2003"), 
+	VBHTP2("E","2004"), CSHARPHTP("F", "2005");
+
 	private final String title;
 	private final String yeah;
 
-	Book(String bookTitle, String yeah)
-	{
+	Book(String bookTitle, String yeah) {
 		title = bookTitle;
 		this.yeah = yeah;
 	}
-	
-	public String getTitle()
-	{
+
+	public String getTitle() {
 		return title;
 	}
-	
-	public String getCopyrightYeah()
-	{
+
+	public String getCopyrightYeah() {
 		return yeah;
 	}
 }
