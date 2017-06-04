8222d7d04fabb14f474e28af96afcef4b6ce6470
DavidSamerson <samerson10@gmail.com>
2017-06-04 12:26:02 -0300

    atributos

--- src/classes/Membros/Static/Employee.java
+++ src/classes/Membros/Static/Employee.java
@@ -1,9 +1,14 @@
 package classes.Membros.Static;
 
 public class Employee {
+	
+	private String firstName;
+	private String lastName;
+	private static int count = 0;
 
 	public Employee() {
 		// TODO Auto-generated constructor stub
+		
 	}
 
 }
