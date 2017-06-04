36e451ecad198af861c53d3271cae2370b3a3f9d
DavidSamerson <samerson10@gmail.com>
2017-06-04 12:33:38 -0300

    construtor terminado

--- src/classes/Membros/Static/Employee.java
+++ src/classes/Membros/Static/Employee.java
@@ -6,9 +6,13 @@ public class Employee {
 	private String lastName;
 	private static int count = 0;
 
-	public Employee() {
+	public Employee(String first, String last) {
 		// TODO Auto-generated constructor stub
+		firstName = first;
+		lastName = last;
 		
+		count ++;
+		System.out.printf("Enployee Construtor: %s %s , Count: %d\n", firstName, lastName, count);
 	}
 
 }
