809f9597ed8b33d1ae8d4fd77ed19e9d910001ef
DavidSamerson <samerson10@gmail.com>
2017-06-05 15:06:06 -0300

    semi finalizada import STATIC

--- src/classes/STATIC/StaticImportTest.java
+++ src/classes/STATIC/StaticImportTest.java
@@ -1,9 +1,16 @@
 package classes.STATIC;
 
+import static java.lang.Math.*;
+
 public class StaticImportTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.printf("Sqrt( 900.0 ) = %.1f\n",sqrt(900));
+		System.out.printf("Ceil ( -9.8 ) = %.1f\n",ceil(-9.8));
+		System.out.printf("Log ( E ) = %.1f\n",log(E));
+		System.out.printf("");
 
 	}
 
