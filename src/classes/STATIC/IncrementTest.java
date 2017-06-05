d4c830776ede9af31c7c97936257bcb76a6c0019
DavidSamerson <samerson10@gmail.com>
2017-06-05 16:54:37 -0300

    incrementTest

---
+++ src/classes/STATIC/IncrementTest.java
@@ -0,0 +1,12 @@
+package classes.STATIC;
+
+public class IncrementTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Increment value = new Increment(5);
+
+	}
+
+}
