4bb2d74d433021306d3238f500c5e200ebedb96c
DavidSamerson <samerson10@gmail.com>
2017-06-05 19:54:41 -0300

    Comit TestDraw

--- src/estudo/GUI/Utilizando/Objetos/Como/Imagem/Grafica/TestDraw.java
+++ src/estudo/GUI/Utilizando/Objetos/Como/Imagem/Grafica/TestDraw.java
@@ -8,9 +8,12 @@ public class TestDraw {
 		// TODO Auto-generated method stub
 		
 		DrawPanel panel = new DrawPanel();
-		JFrame aplication = new JFrame();
+		JFrame application = new JFrame();
 		
-		
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.add(panel);
+		application.setSize(300, 300);
+		application.setVisible(true);
 
 	}
 
