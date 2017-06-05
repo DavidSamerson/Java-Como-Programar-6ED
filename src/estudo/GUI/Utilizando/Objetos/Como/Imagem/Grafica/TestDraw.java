46cc959a0e3adbbe9ed2a626de018111ee99e6c7
DavidSamerson <samerson10@gmail.com>
2017-06-05 19:50:31 -0300

    TesteDraw

---
+++ src/estudo/GUI/Utilizando/Objetos/Como/Imagem/Grafica/TestDraw.java
@@ -0,0 +1,17 @@
+package estudo.GUI.Utilizando.Objetos.Como.Imagem.Grafica;
+
+import javax.swing.JFrame;
+
+public class TestDraw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DrawPanel panel = new DrawPanel();
+		JFrame aplication = new JFrame();
+		
+		
+
+	}
+
+}
