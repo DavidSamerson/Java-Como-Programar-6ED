974c4115359164d8540a27516b6504b814263dd5
DavidSamerson <samerson10@gmail.com>
2017-06-05 17:01:03 -0300

    variaveis de instância final

--- src/classes/STATIC/IncrementTest.java
+++ src/classes/STATIC/IncrementTest.java
@@ -4,8 +4,18 @@ public class IncrementTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Increment value = new Increment(5);
+
+		System.out.printf("Before Incrementing: %s\n\n", value);
+
+		for (int i = 1; i <= 3; i++) {
+			
+			value.addIncrementToTotal();
+			System.out.printf("after Incrementing: %s\n\n", value);
+
+
+		}
 
 	}
 
