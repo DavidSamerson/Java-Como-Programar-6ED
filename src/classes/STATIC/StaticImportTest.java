fc2e9509fc5e1ebc50f9777af5ee90e2f4e29c81
DavidSamerson <samerson10@gmail.com>
2017-06-05 15:07:53 -0300

    classe Terminada, Import STATIC

--- src/classes/STATIC/StaticImportTest.java
+++ src/classes/STATIC/StaticImportTest.java
@@ -10,7 +10,7 @@ public class StaticImportTest {
 		System.out.printf("Sqrt( 900.0 ) = %.1f\n",sqrt(900));
 		System.out.printf("Ceil ( -9.8 ) = %.1f\n",ceil(-9.8));
 		System.out.printf("Log ( E ) = %.1f\n",log(E));
-		System.out.printf("");
+		System.out.printf("Cos ( 0.0 ) = %.1f\n", cos(0.0));
 
 	}
 
