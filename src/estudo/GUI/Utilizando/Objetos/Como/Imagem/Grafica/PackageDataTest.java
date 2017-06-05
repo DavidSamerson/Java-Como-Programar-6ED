d70f942fd7d88858b30ea75fa923c4e8914268e2
DavidSamerson <samerson10@gmail.com>
2017-06-05 17:44:40 -0300

    criação da classe que muda aleatoriamente toda vez que é executada

---
+++ src/estudo/GUI/Utilizando/Objetos/Como/Imagem/Grafica/PackageDataTest.java
@@ -0,0 +1,14 @@
+package estudo.GUI.Utilizando.Objetos.Como.Imagem.Grafica;
+
+public class PackageDataTest {
+
+	public PackageDataTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
