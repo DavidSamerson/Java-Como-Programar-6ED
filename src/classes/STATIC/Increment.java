58630aca5b927c46fabecdd94ecc054c5fe24efe
DavidSamerson <samerson10@gmail.com>
2017-06-05 15:45:51 -0300

    criando a classe STATIC increment

---
+++ src/classes/STATIC/Increment.java
@@ -0,0 +1,14 @@
+package classes.STATIC;
+
+public class Increment {
+	
+	private int total = 0;
+	private final int INCREMENT;
+	
+	public Increment(int incrementValue){
+		INCREMENT = incrementValue;
+	}
+	
+	
+
+}
