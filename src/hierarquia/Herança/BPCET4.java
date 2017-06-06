042c0aedbad277a89bd53c051efbd7526985ca2b
DavidSamerson <samerson10@gmail.com>
2017-06-06 16:10:04 -0300

    criação da classe BPCET4

---
+++ src/hierarquia/Herança/BPCET4.java
@@ -0,0 +1,13 @@
+package hierarquia.Herança;
+
+public class BPCET4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BasePlusCommissionEmployee4 employee = new BasePlusCommissionEmployee4("BOB",
+				"Lewis", "333-33-3333", 5000, .04, 300);
+
+	}
+
+}
