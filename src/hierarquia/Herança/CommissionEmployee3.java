6d89890c10dfbe63d454668ccef0930161588599
DavidSamerson <samerson10@gmail.com>
2017-06-06 14:05:13 -0300

    ce3

---
+++ src/hierarquia/Herança/CommissionEmployee3.java
@@ -0,0 +1,17 @@
+package hierarquia.Herança;
+
+public class CommissionEmployee3 {
+	
+	private String firstName;
+	private String lastName;
+	private String socialSecurityNumber;
+	private Double grossSales;
+	private double commissionRate;
+
+	public CommissionEmployee3() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
