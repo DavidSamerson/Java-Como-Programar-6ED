384706149c462d440c8158da78b9bec718c897ce
DavidSamerson <samerson10@gmail.com>
2017-06-06 16:19:42 -0300

    classe teste terminada

--- src/hierarquia/Herança/BPCET4.java
+++ src/hierarquia/Herança/BPCET4.java
@@ -7,6 +7,20 @@ public class BPCET4 {
 		
 		BasePlusCommissionEmployee4 employee = new BasePlusCommissionEmployee4("BOB",
 				"Lewis", "333-33-3333", 5000, .04, 300);
+		
+		System.out.println("Informações sobre os métodos:");
+		
+		System.out.printf("\nNome: %s %s\n",employee.getFirstName(), employee.getLastName());
+		System.out.printf("Numero Segurança social: %s %s\n",employee.getSocialSecurityNumber());
+		System.out.printf("Gross sales: %s %.2f\n",employee.getGrossSales() );
+		System.out.printf("Commission rate: %s %.2f\n",employee.getCommissionRate() );
+		System.out.printf("Base Salary: %s %.2f\n",employee.getBaseSalary() );
+		
+		employee.setBaseSalary(1000);
+		
+		System.out.printf("\n%s: \n\n%s\n", "Updated employee information",
+				employee.toString());
+		
 
 	}
 
