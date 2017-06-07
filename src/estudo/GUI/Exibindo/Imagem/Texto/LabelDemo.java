a73bebf702fba4139c955f8b702f0df9e043c026
DavidSamerson <samerson10@gmail.com>
2017-06-06 21:51:39 -0300

    imagens e texto com jframe e jlabel

---
+++ src/estudo/GUI/Exibindo/Imagem/Texto/LabelDemo.java
@@ -0,0 +1,29 @@
+package estudo.GUI.Exibindo.Imagem.Texto;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class LabelDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JLabel northLabel = new JLabel("David Samerson");
+		
+		ImageIcon labelIcon = new ImageIcon("sorvete.gif");
+		
+		JLabel centerLabel = new JLabel(labelIcon);
+		
+		JLabel southLabel = new JLabel(labelIcon);
+		
+		southLabel.setText("David Samerson");
+		
+		JFrame application = new JFrame();
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+
+	}
+
+}
