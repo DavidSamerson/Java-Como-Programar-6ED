80bef445529fb06967f40c1746a24d139542ca9c
DavidSamerson <samerson10@gmail.com>
2017-06-07 18:12:33 -0300

    employee

--- src/polimorfismo/Geral/Employee.java
+++ src/polimorfismo/Geral/Employee.java
@@ -5,5 +5,38 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 	private String ssn;
+	
+	public Employee(String first, String last, String ssn){
+		
+		firstName = first;
+		lastName = last;
+		this.ssn = ssn;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	
+	
 
 }
