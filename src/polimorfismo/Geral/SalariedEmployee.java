5f1b348fcd4dcef725c643d299f7dbedc18e5931
DavidSamerson <samerson10@gmail.com>
2017-06-07 18:22:35 -0300

    Classe Salario

---
+++ src/polimorfismo/Geral/SalariedEmployee.java
@@ -0,0 +1,13 @@
+package polimorfismo.Geral;
+
+public class SalariedEmployee extends Employee {
+	
+	private double salary;
+
+	public SalariedEmployee( String firstName, String lastName, String ssn, double salary ) {
+		// TODO Auto-generated constructor stub
+		super(firstName, lastName, ssn);
+		this.salary = salary;
+	}
+
+}
