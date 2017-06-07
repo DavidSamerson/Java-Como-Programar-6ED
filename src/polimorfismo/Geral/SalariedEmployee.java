ef86be155071fc94eb3dc53b26d976d5e02e7952
DavidSamerson <samerson10@gmail.com>
2017-06-07 18:24:15 -0300

    correção

--- src/polimorfismo/Geral/SalariedEmployee.java
+++ src/polimorfismo/Geral/SalariedEmployee.java
@@ -7,7 +7,17 @@ public class SalariedEmployee extends Employee {
 	public SalariedEmployee( String firstName, String lastName, String ssn, double salary ) {
 		// TODO Auto-generated constructor stub
 		super(firstName, lastName, ssn);
+		setSalary(salary);
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
 		this.salary = salary;
 	}
+	
+	
 
 }
