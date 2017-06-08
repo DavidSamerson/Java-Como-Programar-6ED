8bf4753e078d09b44b7ba936019c3188646e1a28
DavidSamerson <samerson10@gmail.com>
2017-06-08 12:30:23 -0300

    criando a classe base

---
+++ src/polimorfismo/Geral/BasePlusCommissionEmployee.java
@@ -0,0 +1,12 @@
+package polimorfismo.Geral;
+
+public class BasePlusCommissionEmployee extends CommissionEmployee {
+	
+	private double baseSalary;
+
+	public BasePlusCommissionEmployee(String first, String last, String ssn, double sales, double rate, double salary) {
+		// TODO Auto-generated constructor stub
+		super(first, last, ssn, sales, rate);
+	}
+
+}
