6f093916504335f9a5dc509ac0f6c80d650c856b
DavidSamerson <samerson10@gmail.com>
2017-06-08 12:37:32 -0300

    BPCE terminada

--- src/polimorfismo/Geral/BasePlusCommissionEmployee.java
+++ src/polimorfismo/Geral/BasePlusCommissionEmployee.java
@@ -1,12 +1,32 @@
 package polimorfismo.Geral;
 
+import java.nio.channels.GatheringByteChannel;
+
 public class BasePlusCommissionEmployee extends CommissionEmployee {
-	
+
 	private double baseSalary;
 
 	public BasePlusCommissionEmployee(String first, String last, String ssn, double sales, double rate, double salary) {
 		// TODO Auto-generated constructor stub
 		super(first, last, ssn, sales, rate);
+		setBaseSalary(salary);
+	}
+
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = (baseSalary < 0.0) ? 0.0 : baseSalary;
+	}
+
+	public double earnings() {
+		return getBaseSalary() + super.earnings();
+	}
+	
+	public String toString(){
+		return String.format("%s %s; %s: $%,.2f","Base-salaried",super.toString(),
+				"Base Salary",getBaseSalary());
 	}
 
 }
