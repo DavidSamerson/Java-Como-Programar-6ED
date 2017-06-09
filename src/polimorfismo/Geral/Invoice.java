be83867d0c21dabbd190022a34c2093dfa49aefa
DavidSamerson <samerson10@gmail.com>
2017-06-09 16:42:51 -0300

    classe invoice

---
+++ src/polimorfismo/Geral/Invoice.java
@@ -0,0 +1,72 @@
+package polimorfismo.Geral;
+
+public class Invoice implements Payable {
+	
+	private String partNumber;
+	private String partDescription;
+	private int quantity;
+	private double princePerItem;
+
+	public Invoice(String part, String description, int count, double price) {
+		// TODO Auto-generated constructor stub
+		partNumber = part;
+		partDescription = description;
+		setQuantity(count);
+		setPrincePerItem(price);
+	}
+
+	public String getPartNumber() {
+		return partNumber;
+	}
+
+
+
+	public void setPartNumber(String partNumber) {
+		this.partNumber = partNumber;
+	}
+
+
+
+	public String getPartDescription() {
+		return partDescription;
+	}
+
+
+
+	public void setPartDescription(String partDescription) {
+		this.partDescription = partDescription;
+	}
+
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity < 0 ? 0 : quantity;
+	}
+
+
+
+	public double getPrincePerItem() {
+		return princePerItem;
+	}
+
+
+
+	public void setPrincePerItem(double princePerItem) {
+		this.princePerItem = princePerItem < 0.0 ? 0.0 : princePerItem;
+	}
+
+
+
+	@Override
+	public double getPaymentAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
