9faca4e676bd74805148af97620a29bab1654f64
DavidSamerson <samerson10@gmail.com>
2017-06-09 18:37:47 -0300

    SE2 terminado

--- src/polimorfismo/Geral/SalariedEmployee2.java
+++ src/polimorfismo/Geral/SalariedEmployee2.java
@@ -1,9 +1,31 @@
 package polimorfismo.Geral;
 
-public class SalariedEmployee2 {
+public class SalariedEmployee2 extends Employee2{
+	
+	private double weeklySalary;
 
-	public SalariedEmployee2() {
+	public SalariedEmployee2(String first, String last,String ssn, double salary) {
 		// TODO Auto-generated constructor stub
+		super(first, last, ssn);
+		setWeeklySalary(salary);
+	}
+	
+	public double getWeeklySalary() {
+		return weeklySalary;
+	}
+
+	public void setWeeklySalary(double weeklySalary) {
+		this.weeklySalary = weeklySalary < 0.0 ? 0.0 : weeklySalary;
+	}
+
+	@Override
+	public double getPaymentAmount() {
+		// TODO Auto-generated method stub
+		return getWeeklySalary();
+	}
+	
+	public String toString(){
+		return String.format("Salaried Employee: %s\n%s: $%,.2f", super.toString(),"Weekly Salary", getWeeklySalary());
 	}
 
 }
