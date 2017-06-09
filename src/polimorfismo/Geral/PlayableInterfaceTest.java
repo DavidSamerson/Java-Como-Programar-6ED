f49e84992427bea02fe29923cb1fcd28d2cb99f3
DavidSamerson <samerson10@gmail.com>
2017-06-09 19:02:36 -0300

    classe teste terminada

--- src/polimorfismo/Geral/PlayableInterfaceTest.java
+++ src/polimorfismo/Geral/PlayableInterfaceTest.java
@@ -11,7 +11,12 @@ public class PlayableInterfaceTest {
 		payableObjects[2] = new SalariedEmployee2("jonh", "seat", "111-111-1111", 800.00);
 		payableObjects[3] = new SalariedEmployee2("lisa", "seat", "888-888-8888", 1300.00);
 		
+		System.out.println("Polimorficamente falando: ");
 		
+		for(Payable currentPayable : payableObjects){
+			System.out.printf("%s \n%s: $%,.2f\n\n", currentPayable.toString(),
+					"Payment duo", currentPayable.getPaymentAmount());
+		}
 
 	}
 
