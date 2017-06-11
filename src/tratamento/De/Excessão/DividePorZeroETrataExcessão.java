e0eaf3be853358bab30b9705ae6186dc091c52fa
DavidSamerson <samerson10@gmail.com>
2017-06-11 15:36:48 -0300

    criação de uma classe que utiliza excessão

---
+++ src/tratamento/De/Excessão/DividePorZeroETrataExcessão.java
@@ -0,0 +1,19 @@
+package tratamento.De.Excessão;
+
+import java.util.Scanner;
+
+public class DividePorZeroETrataExcessão {
+	
+	public static int DivideZerobyException(int numerador, int denominador) throws ArithmeticException {
+		return numerador / denominador;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+
+	}
+
+}
