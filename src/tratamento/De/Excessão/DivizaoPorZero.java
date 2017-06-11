ce38b3e23805d582f84439bb936f6cdd67ae221c
DavidSamerson <samerson10@gmail.com>
2017-06-11 15:13:27 -0300

    classe demonstrativa divisão por zero

---
+++ src/tratamento/De/Excessão/DivizaoPorZero.java
@@ -0,0 +1,9 @@
+package tratamento.De.Excessão;
+
+public class DivizaoPorZero {
+
+	// TODO Auto-generated method stub
+
+	public static int quotient(int numerador, int denominador){
+		return numerador/denominador;}
+}
