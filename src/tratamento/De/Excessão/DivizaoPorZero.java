cd2b6e0667bae16cc53f9be0baa79e727d82ac08
DavidSamerson <samerson10@gmail.com>
2017-06-11 15:23:02 -0300

    divisã]opor zero terminada

--- src/tratamento/De/Excessão/DivizaoPorZero.java
+++ src/tratamento/De/Excessão/DivizaoPorZero.java
@@ -6,19 +6,21 @@ public class DivizaoPorZero {
 
 	// TODO Auto-generated method stub
 
-	public static int quotient(int numerador, int denominador){
-		return numerador/denominador;
+	public static int quotient(int numerador, int denominador) {
+		return numerador / denominador;
 	}
-	
+
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner (System.in);
-		
+
+		Scanner sc = new Scanner(System.in);
+
 		System.out.printf("Entre com o numerador");
 		int numerador = sc.nextInt();
 		System.out.printf("Entre com o denominador");
 		int denominador = sc.nextInt();
-		
-		
+
+		int result = quotient(numerador, denominador);
+
+		System.out.printf("\nResultado: %d / %d  = %d\n", numerador, denominador, result);
 	}
 }
