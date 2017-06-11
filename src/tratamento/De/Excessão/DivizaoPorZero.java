1e968974557d043ae93dad90c588f74724f3821d
DavidSamerson <samerson10@gmail.com>
2017-06-11 15:20:43 -0300

    divisão por zero demonstrativo

--- src/tratamento/De/Excessão/DivizaoPorZero.java
+++ src/tratamento/De/Excessão/DivizaoPorZero.java
@@ -1,9 +1,24 @@
 package tratamento.De.Excessão;
 
+import java.util.Scanner;
+
 public class DivizaoPorZero {
 
 	// TODO Auto-generated method stub
 
 	public static int quotient(int numerador, int denominador){
-		return numerador/denominador;}
+		return numerador/denominador;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.printf("Entre com o numerador");
+		int numerador = sc.nextInt();
+		System.out.printf("Entre com o denominador");
+		int denominador = sc.nextInt();
+		
+		
+	}
 }
