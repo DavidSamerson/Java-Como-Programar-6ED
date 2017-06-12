d042dc886daf06bcbb59473a21563f19d30baf2a
DavidSamerson <samerson10@gmail.com>
2017-06-12 20:01:09 -0300

    exceções Encadeadas

---
+++ src/tratamento/De/Excessão/ExcessaoEncadeada.java
@@ -0,0 +1,42 @@
+package tratamento.De.Excessão;
+
+public class ExcessaoEncadeada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			
+			method1();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void method1()throws Exception {
+		
+		try {
+			method2();
+		} catch (Exception e)  {
+			throw new Exception("Erro no método 1");
+		}
+		
+	}
+
+	private static void method2() throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			method3();
+		} catch (Exception e) {
+			throw new Exception("Erro no método 2");
+		}
+	}
+
+	private static void method3() throws Exception {
+		// TODO Auto-generated method stub
+		throw new Exception("Erro no método 3");
+	}
+
+}
