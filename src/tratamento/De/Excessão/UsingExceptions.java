b8249dcefdd01ef598a61924051dcfeba9de058b
DavidSamerson <samerson10@gmail.com>
2017-06-12 17:55:11 -0300

    metodo main

--- src/tratamento/De/Excessão/UsingExceptions.java
+++ src/tratamento/De/Excessão/UsingExceptions.java
@@ -4,7 +4,19 @@ public class UsingExceptions {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		try {
+			throwException();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Erro na classe Main");
+		}
 
+	}
+
+	private static void throwException() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
