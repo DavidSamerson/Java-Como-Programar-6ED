51abcdf271ac47ed2a4cdf59e0bceb4772ecb211
DavidSamerson <samerson10@gmail.com>
2017-06-12 20:01:43 -0300

    correção

--- src/tratamento/De/Excessão/ExcecaoEncadeada.java
+++ src/tratamento/De/Excessão/ExcecaoEncadeada.java
@@ -1,6 +1,6 @@
 package tratamento.De.Excessão;
 
-public class ExcessaoEncadeada {
+public class ExcecaoEncadeada {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
