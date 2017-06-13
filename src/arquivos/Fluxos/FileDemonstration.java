3b041e98d0450f8a96b2c20c6bf5689f58f42e9f
DavidSamerson <samerson10@gmail.com>
2017-06-13 15:35:52 -0300

    modificação

--- src/arquivos/Fluxos/FileDemonstration.java
+++ src/arquivos/Fluxos/FileDemonstration.java
@@ -10,7 +10,7 @@ public class FileDemonstration {
 
 		if (name.exists()) {
 
-			System.out.printf("%s%s\n%s\n%s\n%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s", name.getName(), "Exists",
+			System.out.printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", name.getName(), "Exists",
 					(name.isFile() ? "Is a file" : "is not a file"),
 					(name.isDirectory() ? "Is a directory" : "Is not a directory"),
 					(name.isAbsolute() ? "Is a absolute path" : "Is not a absolute path"), "Last Modified: ",
