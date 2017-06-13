943750f57500223fcdcde45b0d167bade23d5392
DavidSamerson <samerson10@gmail.com>
2017-06-13 15:06:43 -0300

    arquivos e fluxos

--- src/arquivos/Fluxos/FileDemonstration.java
+++ src/arquivos/Fluxos/FileDemonstration.java
@@ -1,10 +1,23 @@
 package arquivos.Fluxos;
 
+import java.io.File;
+
 public class FileDemonstration {
 	
 	public void analizePath(String path){
 		
+		File name = new File(path);
 		
+		if (name.exists()){
+			
+			System.out.printf("%s%s\n%s\n%s\n%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s",
+					name.getName(), "Exists", 
+					(name.isFile() ? "Is a file" : "is not a file"),
+					(name.isDirectory() ? "Is a directory" : "Is not a directory"),
+					(name.isAbsolute() ? "Is a absolute path" : "Is not a absolute path"),
+					"Last Modified: ",name.lastModified() );
+			
+		}
 		
 	}
 
