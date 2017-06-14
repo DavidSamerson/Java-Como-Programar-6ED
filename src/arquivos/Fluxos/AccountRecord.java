a783ce86266f7f9c3e5f645fc2ca3dfaff54f7ec
DavidSamerson <samerson10@gmail.com>
2017-06-14 13:31:20 -0300

    classe terminada

--- src/arquivos/Fluxos/AccountRecord.java
+++ src/arquivos/Fluxos/AccountRecord.java
@@ -13,6 +13,45 @@ public class AccountRecord {
 
 	public AccountRecord(int i, String string, String string2, double d) {
 		// TODO Auto-generated constructor stub
+		setAccount(i);
+		setFirstName(string);
+		setLastName(string2);
+		setBalance(d);
+		
 	}
+
+	public int getAccount() {
+		return account;
+	}
+
+	public void setAccount(int account) {
+		this.account = account;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
 
 }
