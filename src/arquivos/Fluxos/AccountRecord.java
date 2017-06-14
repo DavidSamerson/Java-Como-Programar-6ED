6277295e364141635c301ffa533f697fd766364e
DavidSamerson <samerson10@gmail.com>
2017-06-14 13:24:03 -0300

    Teste de fluxo de arquivos

---
+++ src/arquivos/Fluxos/AccountRecord.java
@@ -0,0 +1,18 @@
+package arquivos.Fluxos;
+
+public class AccountRecord {
+	
+	private int account;
+	private String firstName;
+	private String lastName;
+	private double balance;
+
+	public AccountRecord() {
+		this(0,"","",0.0);
+	}
+
+	public AccountRecord(int i, String string, String string2, double d) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
