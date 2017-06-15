6947df2028f60a1d3d295abf063a9b4a9cb595c4
DavidSamerson <samerson10@gmail.com>
2017-06-15 19:26:10 -0300

    serialização de objetos

---
+++ src/arquivos/Fluxos/AccountRecordSerializable.java
@@ -0,0 +1,50 @@
+package arquivos.Fluxos;
+
+import java.io.Serializable;
+
+public class AccountRecordSerializable implements Serializable {
+
+	private int account;
+	private String firstName;
+	private String lastName;
+	private double balance;
+	
+	public AccountRecordSerializable(){
+		this(0,"","", 0.0);
+	}
+	public AccountRecordSerializable(int acct, String first, String last, double bal){
+		
+		setAccount(acct);
+		setFirstName(first);
+		setLastName(last);
+		setBalance(bal);
+		
+	}
+	public int getAccount() {
+		return account;
+	}
+	public void setAccount(int account) {
+		this.account = account;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+
+}
