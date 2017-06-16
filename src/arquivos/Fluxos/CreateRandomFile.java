22e0079e5c918331a7812e6586e0a16139c04fb0
DavidSamerson <samerson10@gmail.com>
2017-06-16 16:40:50 -0300

    classe teste e modificações

--- src/arquivos/Fluxos/CreateRandomFile.java
+++ src/arquivos/Fluxos/CreateRandomFile.java
@@ -4,30 +4,30 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class CreateRandomFile {
-	
+
 	private static final int NUMBER_RECORDS = 100;
-	
-	public void createFile(){
-		
+
+	public void createFile() {
+
 		RandomAccessFile file = null;
-		
+
 		try {
-			
+
 			file = new RandomAccessFile("clients.bat", "rw");
 			RandomAcessAccountRecord record = new RandomAcessAccountRecord();
-			
+
 			for (int i = 0; i < NUMBER_RECORDS; i++) {
-				
+
 				record.write(file);
 				System.out.println("clients.bat criado com sucesso..\n");
 				System.exit(0);
-				
+
 			}
 		} catch (IOException e) {
-			
+
 			System.err.println("Erro ao processar o arquivo..\n");
 			System.exit(1);
-			
+
 		} finally {
 			try {
 				if (file != null)
@@ -37,8 +37,5 @@ public class CreateRandomFile {
 				System.exit(1);
 			}
 		}
-		
 	}
-	
-
 }
---
+++ src/arquivos/Fluxos/CreateRandomFileTest.java
@@ -0,0 +1,12 @@
+package arquivos.Fluxos;
+
+public class CreateRandomFileTest {
+
+	public static void main(String[] args) {
+		
+		CreateRandomFile app = new CreateRandomFile();
+		app.createFile();
+
+	}
+
+}
