4d2c505a2702cb5374f6b5c343e056a1b5231d2e
DavidSamerson <samerson10@gmail.com>
2017-06-16 16:24:41 -0300

    createRandomFile

--- src/arquivos/Fluxos/CreateRandomFile.java
+++ src/arquivos/Fluxos/CreateRandomFile.java
@@ -1,12 +1,41 @@
 package arquivos.Fluxos;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class CreateRandomFile {
 	
 	private static final int NUMBER_RECORDS = 100;
 	
 	public void createFile(){
 		
+		RandomAccessFile file = null;
 		
+		try {
+			
+			file = new RandomAccessFile("clients.bat", "rw");
+			RandomAcessAccountRecord record = new RandomAcessAccountRecord();
+			
+			for (int i = 0; i < NUMBER_RECORDS; i++) {
+				
+				record.write(file);
+				System.out.println("clients.bat criado com sucesso..\n");
+				System.exit(0);
+				
+			}
+		} catch (IOException e) {
+			
+			System.err.println("Erro ao processar o arquivo..\n");
+			System.exit(1);
+			
+		} finally {
+			try {
+				if (file != null)
+					file.close();
+			} catch (Exception e2) {
+				
+			}
+		}
 		
 	}
 	
