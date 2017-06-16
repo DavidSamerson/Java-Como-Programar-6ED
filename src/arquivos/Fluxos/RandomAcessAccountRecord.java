32e3eda82402a2091be10bb9f3b64baa065e1b70
DavidSamerson <samerson10@gmail.com>
2017-06-16 15:22:04 -0300

    final da classe RAAR - acesso aleatório

--- src/arquivos/Fluxos/RandomAcessAccountRecord.java
+++ src/arquivos/Fluxos/RandomAcessAccountRecord.java
@@ -1,9 +1,62 @@
 package arquivos.Fluxos;
 
-public class RandomAcessAccountRecord {
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.RandomAccess;
+
+public class RandomAcessAccountRecord extends AccountRecord {
+
+	public static final int SIZE = 72;
 
 	public RandomAcessAccountRecord() {
-		// TODO Auto-generated constructor stub
+		super (0,"","",0.0);
 	}
-
+	
+	public RandomAcessAccountRecord(int account, String first, String last, double balance) {
+		super(account,first,last,balance);
+	}
+	
+	public void read(RandomAccessFile file) throws IOException {
+		
+		setAccount(file.readInt());
+		setFirstName(readName(file));
+		setLastName(readName(file));
+		setBalance(file.readDouble());
+		
+	}
+	
+	private String readName(RandomAccessFile file) throws IOException {
+		
+		char name[] = new char[15],temp;
+		
+		for (int i = 0; i < name.length; i++) {
+			temp = file.readChar();
+			name[i] = temp;
+		}
+		
+		return new String(name).replace('\0',' ');
+	}
+	
+	public void write(RandomAccessFile file) throws IOException{
+		
+		file.writeInt(getAccount());
+		writeName(file,getFirstName());
+		writeName(file,getLastName());
+		file.writeDouble(getBalance());
+	}
+	
+	private void writeName(RandomAccessFile file, String name ) throws IOException{
+		
+		StringBuffer buffer = null;
+		
+		if (name != null)
+			buffer = new StringBuffer(name);
+		else
+			buffer = new StringBuffer(15);
+		
+		buffer.setLength(15);
+		file.writeChars(buffer.toString());
+		
+	}
+	
 }
