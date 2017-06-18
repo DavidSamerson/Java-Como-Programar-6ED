c3ce41d99b1c8de23f7fa201c53dbec1529b4211
DavidSamerson <samerson10@gmail.com>
2017-06-18 15:50:40 -0300

    classe teste

---
+++ src/arquivos/Fluxos/FileDemonstrationTest2.java
@@ -0,0 +1,13 @@
+package arquivos.Fluxos;
+
+import javax.swing.JFrame;
+
+public class FileDemonstrationTest2 {
+
+	public static void main(String[] args) {
+		
+		FileDemonstrator app = new FileDemonstrator();
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
