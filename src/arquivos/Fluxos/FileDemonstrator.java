2ee3a9f2393ab510df221c905826f918d877851d
DavidSamerson <samerson10@gmail.com>
2017-06-18 15:05:55 -0300

    método getFile

--- src/arquivos/Fluxos/FileDemonstrator.java
+++ src/arquivos/Fluxos/FileDemonstrator.java
@@ -1,18 +1,47 @@
 package arquivos.Fluxos;
 
+import java.awt.BorderLayout;
 import java.io.File;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class FileDemonstrator extends JFrame {
 	
 	private JTextArea outputArea;
-	private JScrollPane scrollPanel;
+	private JScrollPane scrollPane;
 	
-	public FileDemonstrator(){}
-	private File getFile(){}
+	public FileDemonstrator()
+	{
+		super("Testando a classe");
+		outputArea = new JTextArea();
+		scrollPane = new JScrollPane(outputArea);
+		add(scrollPane, BorderLayout.CENTER);
+		setSize(400,400);
+		setVisible(true);
+		analysePath();
+	}
+	
+	private File getFile()
+	{
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		int result = fileChooser.showOpenDialog(this);
+		if (result == JFileChooser.CANCEL_OPTION) 
+		{
+			System.exit(1);
+		}
+		File fileName = fileChooser.getSelectedFile();
+		if ((fileName == null)||(fileName.getName().equals(""))) 
+		{
+			JOptionPane.showMessageDialog(this, "Invalid File Name",
+					"Invalid File Name", JOptionPane.ERROR_MESSAGE);
+		}
+		return fileName;
+	}
 	public void analysePath(){}
 
 }
