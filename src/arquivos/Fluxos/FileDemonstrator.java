a0a827651dd375cc813660f9b8160ea5d04fd630
DavidSamerson <samerson10@gmail.com>
2017-06-18 14:44:52 -0300

    classe quase terminada

--- src/arquivos/Fluxos/FileDemonstrator.java
+++ src/arquivos/Fluxos/FileDemonstrator.java
@@ -1,9 +1,18 @@
 package arquivos.Fluxos;
 
+import java.io.File;
+
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class FileDemonstrator extends JFrame {
 	
+	private JTextArea outputArea;
+	private JScrollPane scrollPanel;
 	
+	public FileDemonstrator(){}
+	private File getFile(){}
+	public void analysePath(){}
 
 }
