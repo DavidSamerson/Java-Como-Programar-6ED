1c01ec83785a016d365e8704b5b4f8117911bd75
DavidSamerson <samerson10@gmail.com>
2017-06-19 14:39:11 -0300

    fim da classe

--- src/recursão/TorreDeHanoi.java
+++ src/recursão/TorreDeHanoi.java
@@ -1,15 +1,23 @@
 package recursão;
 
 public class TorreDeHanoi {
-	
+
 	int numDisk;
-	
-	public TorreDeHanoi(int disk, int sourcePag, int destinationPeg, int tempPeg){
-		
+
+	public TorreDeHanoi(int disk) {
+		numDisk = disk;
+	}
+
+	public void solveTowers(int disk, int sourcePag, int destinationPeg, int tempPeg) {
+
 		if (disk == 1) {
 			System.out.printf("\n%d ---> %d", sourcePag, destinationPeg);
 			return;
 		}
+
+		solveTowers(disk - 1, sourcePag, tempPeg, destinationPeg);
+		System.out.printf("\n%d ---> %d", sourcePag, destinationPeg);
+		solveTowers(disk - 1, tempPeg, destinationPeg,sourcePag);
 	}
 
 }
