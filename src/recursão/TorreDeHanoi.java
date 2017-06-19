e71fe79a859541cd470df31de35fc05f0c760b7b
DavidSamerson <samerson10@gmail.com>
2017-06-19 14:35:41 -0300

    t

--- src/recursão/TorreDeHanoi.java
+++ src/recursão/TorreDeHanoi.java
@@ -2,6 +2,14 @@ package recursão;
 
 public class TorreDeHanoi {
 	
+	int numDisk;
 	
+	public TorreDeHanoi(int disk, int sourcePag, int destinationPeg, int tempPeg){
+		
+		if (disk == 1) {
+			System.out.printf("\n%d ---> %d", sourcePag, destinationPeg);
+			return;
+		}
+	}
 
 }
