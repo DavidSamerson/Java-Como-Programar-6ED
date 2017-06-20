915724c79ad60f78cdad2afc73f824fd2394f4b7
DavidSamerson <samerson10@gmail.com>
2017-06-20 16:07:56 -0300

    classe teste terminada

--- src/pesquisa/E/Classificação/PesquisaBinariaTeste.java
+++ src/pesquisa/E/Classificação/PesquisaBinariaTeste.java
@@ -1,11 +1,38 @@
 package pesquisa.E.Classificação;
 
+import java.util.Scanner;
+
 public class PesquisaBinariaTeste {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
 		
-
+		int searchInt;
+		int position;
+		
+		PesquisaBinaria app = new PesquisaBinaria(15);
+		System.out.println(app);
+		
+		System.out.println("entre com uma pesquisa (-1 sai)");
+		searchInt = input.nextInt();
+		System.out.println();
+		
+		while (searchInt != -1) {
+			
+			position = app.binarySearch(searchInt);
+			
+			if (position ==  -1) {
+				System.out.println("Não encontrado");
+			}
+			
+			else
+				System.out.println("Encontrado na posição " + position+ "\n");
+			
+			System.out.println("entre com uma pesquisa (-1 sai)");
+			searchInt = input.nextInt();
+			System.out.println();
+		}
 	}
 
 }
