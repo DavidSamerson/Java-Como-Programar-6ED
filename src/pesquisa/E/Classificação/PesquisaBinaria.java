91e31f7a8a591dbfdb767a7d1b8791b6cbdfba44
DavidSamerson <samerson10@gmail.com>
2017-06-20 15:21:33 -0300

    Pesquisa binária

---
+++ src/pesquisa/E/Classificação/PesquisaBinaria.java
@@ -0,0 +1,28 @@
+package pesquisa.E.Classificação;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class PesquisaBinaria {
+	
+	private int [] data;
+	private static Random gerator = new Random();
+
+	public PesquisaBinaria(int size)
+	{
+			data = new int[size];
+			for (int i = 0; i < size; i++) {
+				data[i] = 10 + gerator.nextInt(90);
+			}
+			Arrays.sort(data);
+	}
+	
+	public int binarySearch(int searchElement)
+	{
+		int low = 0;
+		int righ = data.length - 1;
+		int middle = (low + righ + 1) / 2;
+		int location = -1;
+	}
+
+}
