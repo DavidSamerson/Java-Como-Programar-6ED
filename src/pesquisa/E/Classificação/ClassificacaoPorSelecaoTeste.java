a79bc12a26db653b9acc20bd7b0390982e178eee
DavidSamerson <samerson10@gmail.com>
2017-06-21 15:32:11 -0300

    classe testeterminado

--- src/pesquisa/E/Classificação/ClassificacaoPorSelecaoTeste.java
+++ src/pesquisa/E/Classificação/ClassificacaoPorSelecaoTeste.java
@@ -5,7 +5,15 @@ public class ClassificacaoPorSelecaoTeste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		ClassificacaoPorSelecao app = new ClassificacaoPorSelecao(10);
 		
+		System.out.print("Array: \n");
+		System.out.print(app);
+		
+		app.sort();
+		
+		System.out.print("Array: \n");
+		System.out.print(app);
 
 	}
 
