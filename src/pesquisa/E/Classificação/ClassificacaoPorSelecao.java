8165a694247b0456900e3aca5f1b92e28fc22d8a
DavidSamerson <samerson10@gmail.com>
2017-06-21 15:14:27 -0300

    classificação por seleção

---
+++ src/pesquisa/E/Classificação/ClassificacaoPorSelecao.java
@@ -0,0 +1,55 @@
+package pesquisa.E.Classificação;
+
+import java.util.Random;
+
+public class ClassificacaoPorSelecao {
+	
+	private int [] data;
+	private static Random generator = new Random();
+
+	public ClassificacaoPorSelecao(int size) 
+	{
+		data = new int[size];
+		for (int i = 0; i < size; i++) {
+			data[i] = 10 + generator.nextInt(90);
+		}
+	}
+	
+	public void sort()
+	{
+		int smallest;
+		
+		for (int i = 0; i < data.length -1; i++) {
+			smallest = i;
+			for (int j = i+1; j < data.length ; j++) {
+				if (data[j]<data[smallest])
+					smallest = j;
+			}
+			swap(i,smallest);
+			printPass(i+1, smallest);
+		}
+	}
+
+	private void printPass(int pass, int index) {
+		// TODO Auto-generated method stub
+		System.out.print(String.format("After Pass %2d: ", pass));
+		
+		for (int i = 0; i < index; i++) {
+			System.out.print(data[i] + " ");
+		}
+		System.out.print(data[index] + "* ");
+		
+		for (int i = 0; i < data.length; i++) {
+			System.out.print(data[i] + " ");
+		}
+		System.out.println("\n        ");
+	}
+
+	private void swap(int first, int second) {
+		// TODO Auto-generated method stub
+		int temporary = data[first];
+		data[first] = data[second];
+		data[second] = temporary;
+	}
+
+}
