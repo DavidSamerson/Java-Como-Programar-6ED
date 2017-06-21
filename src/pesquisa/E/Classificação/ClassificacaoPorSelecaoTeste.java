914c81d91d2965e495a4a13d64c05b36ab92e959
DavidSamerson <samerson10@gmail.com>
2017-06-21 15:29:45 -0300

    classe teste

---
+++ src/pesquisa/E/Classificação/ClassificacaoPorSelecaoTeste.java
@@ -0,0 +1,12 @@
+package pesquisa.E.Classificação;
+
+public class ClassificacaoPorSelecaoTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
