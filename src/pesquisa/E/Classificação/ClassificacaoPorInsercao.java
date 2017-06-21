ace9453618dd82c7c9f019326de6c8566e81737b
DavidSamerson <samerson10@gmail.com>
2017-06-21 16:06:58 -0300

    classificação por seleção terminada

--- src/pesquisa/E/Classificação/ClassificacaoPorInsercao.java
+++ src/pesquisa/E/Classificação/ClassificacaoPorInsercao.java
@@ -6,14 +6,55 @@ public class ClassificacaoPorInsercao {
 
 	private int[] data;
 	private static Random gerador = new Random();
-	
+
 	public ClassificacaoPorInsercao(int size) {
-		data = new int [size];
+		data = new int[size];
 		for (int i = 0; i < size; i++) {
-			 data[i] = 10 + gerador.nextInt(90);
+			data[i] = 10 + gerador.nextInt(90);
+		}
+	}
+
+	public void sort() {
+		int insert;
+		for (int i = 1; i < data.length; i++) {
+			insert = data[i];
+			int moverItem = i;
+			while (moverItem > 0 && data[moverItem - 1] > insert) {
+				data[moverItem] = data[moverItem-1];
+				moverItem--;
+			}
+			data[moverItem] = insert;
+			printPass(i,moverItem);
 		}
 	}
 	
+	private void printPass(int pass, int index) {
+		// TODO Auto-generated method stub
+		System.out.print(String.format("After Pass %2d: ", pass));
+		
+		for (int i = 0; i < index; i++) {
+			System.out.print(data[i] + "  ");
+		}
+		System.out.print(data[index] + "* ");
+		
+		for (int i = 0; i < data.length; i++) {
+			System.out.print(data[i] + "  ");
+		}
+		System.out.println("\n            ");
+		
+		for (int i = 0; i < pass; i++) {
+			System.out.print("-- ");
+		}
+		System.out.println("\n");
+	}
 	
+	public String toString(){
+		StringBuffer temporary = new StringBuffer();
+		for (int i : data) {
+			temporary.append(i + " ");
+		}
+		temporary.append("\n");
+		return temporary.toString();
+	}
 
 }
