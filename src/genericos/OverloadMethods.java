85eec8608541807eaf34872e1a6257a7ace42edd
DavidSamerson <samerson10@gmail.com>
2017-06-23 15:32:48 -0300

    classe

--- src/genericos/OverloadMethods.java
+++ src/genericos/OverloadMethods.java
@@ -2,21 +2,41 @@ package genericos;
 
 public class OverloadMethods {
 	
-	public static void printArray(Integer[] inputArray){
+	public static void printArray(Integer[] inputArray)
+	{
 		for(Integer element : inputArray){
 			System.out.printf("%s",inputArray);
 		}
+		System.out.println();
 	}
 	
-	public static void printArray(Double[] inputArray){
+	public static void printArray(Double[] inputArray)
+	{
 		for(Double element : inputArray){
 			System.out.printf("%s",inputArray);
 		}
+		System.out.println();
 	}
 	
-	public static void printArray(Character[] inputArray){
+	public static void printArray(Character[] inputArray)
+	{
 		for(Character element : inputArray){
 			System.out.printf("%s",inputArray);
 		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args)
+	{
+		Integer[] integerArray = {1,2,3,4,5,6};
+		Double[] doubleArray = {1.1,1.2,1.3,1.4,1.5,1.6,1.7};
+		Character[]characterArray = {'h','e','l','l','o'};
+		
+		System.out.println("Array de Inteiros");
+		printArray(integerArray);
+		System.out.println("Array de Double");
+		printArray(doubleArray);
+		System.out.println("Array de Caracteres");
+		printArray(characterArray);
 	}
 }
