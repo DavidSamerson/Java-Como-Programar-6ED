f68f8f6c1ddd291f2193bb8997fdcef611f260ea
DavidSamerson <samerson10@gmail.com>
2017-06-25 14:40:02 -0300

    classe genérica

---
+++ src/genericos/FullStackException.java
@@ -0,0 +1,9 @@
+package genericos;
+
+public class FullStackException extends Exception {
+
+	public FullStackException(String format) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/genericos/Stack.java
+++ src/genericos/Stack.java
@@ -2,8 +2,27 @@ package genericos;
 
 public class Stack<E> {
 
+	private final int size;
+	private int top;
+	private E[] elements;
+
+	public Stack() {
+		this(10);
+	}
+
+	public Stack(int s) {
+		size = s > 0 ? s : 10;
+		top = -1;
+		elements = (E[]) new Object[size];
+	}
+
+	public void push(E pushValue){
+		if(top == size-1) throw new FullStackException(String.format("Pilha cheia %s", pushValue));
+		elements[++top] = pushValue;
+	}
+	
+
 	public static void main(String[] args) {
-		
 
 	}
 
