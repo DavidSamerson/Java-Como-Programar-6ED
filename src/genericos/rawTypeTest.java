24e486883ac6c19d78c958660c3231fab2eef4bd
DavidSamerson <samerson10@gmail.com>
2017-06-26 17:25:25 -0300

    classe teste terminada

--- src/genericos/rawTypeTest.java
+++ src/genericos/rawTypeTest.java
@@ -1,5 +1,7 @@
 package genericos;
 
+import java.util.Iterator;
+
 public class rawTypeTest {
 	
 	private Double doubleElements[] = {1.1,2.2,3.3,4.4,5.5,6.6};
@@ -20,21 +22,43 @@ public class rawTypeTest {
 		
 	}
 
-	private void testPop(String string, Stack stack1) {
-		// TODO Auto-generated method stub
+	private <T> void testPop(String string, Stack <T> stack) 
+	{
+		try {
+			System.out.printf("\nPopping elements from %s\n",string);
+			T popper;
+			
+			while (true) {
+				popper = stack.pop();
+				System.out.printf("%s",popper);
+			}
+		} catch (EmptyStackException e) {
+			System.out.println();
+			e.printStackTrace();
+		}
 		
 	}
 
-	private <T> void testPush(String string, Stack <T> stack1, T[] doubleElements2) {
+	private <T> void testPush(String name, Stack <T> stack, T[] doubleElements2)
+	{
 		
-		
+		try {
+			System.out.printf("\nPushing Elements onto %s\n",name);
+			for (T t : doubleElements2) {
+				System.out.printf("%s", t);
+				stack.push(t);
+			}
+		} catch (FullStackException e) {
+			System.out.println();
+			e.printStackTrace();
+		}
 		
 	}
 
 	public static void main(String[] args) {
 		
-		
-
+		rawTypeTest app = new rawTypeTest();
+		app.testStack();
 	}
 
 }
