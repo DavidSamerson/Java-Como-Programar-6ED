6fb824602aa3fe53a8fcc45fb2f91560dbf7068e
DavidSamerson <samerson10@gmail.com>
2017-06-27 14:34:37 -0300

    classe ensinando a coleções e as classes ARRAY

---
+++ src/colecoes/UsingArrays.java
@@ -0,0 +1,17 @@
+package colecoes;
+
+public class UsingArrays {
+	
+	private int intArray[] = {1,2,3,4,5,6};
+	private double doubleArray[] = {8.4, 9.3, 0.2, 7.9, 3.4};
+
+	public UsingArrays() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
