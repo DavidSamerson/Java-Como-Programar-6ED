0807c84b7bf3e37aff023c6819c577c398c1dcf1
DavidSamerson <samerson10@gmail.com>
2017-06-29 15:29:56 -0300

    testando a classe Vectors

---
+++ src/colecoes/VectorTest.java
@@ -0,0 +1,59 @@
+package colecoes;
+
+import java.util.NoSuchElementException;
+import java.util.Vector;
+
+public class VectorTest {
+	
+	private static final String colors[] = {"RED", "WHITE", "BLUE"};
+
+	public VectorTest() {
+		
+		Vector<String> vector = new Vector<String> ();
+		printVector(vector);
+		
+		for (String a : vector) {
+			vector.add(a);
+		}
+		
+		printVector(vector);
+		
+		try {
+			
+			System.out.printf("First Elements: %s\n",vector.firstElement());
+			System.out.printf("Last Elements: %s\n",vector.lastElement());
+			
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		}
+		
+		if(vector.contains("RED"))
+			System.out.printf("\n\"RED\" notFound at index %d\n",
+			vector.indexOf("RED"));
+		else
+			System.out.printf("\n\"RED\" notFound \n ");
+		
+		vector.remove("RED");
+		System.out.println("Vector RED as been removed");
+		printVector(vector);
+		
+		if(vector.contains("RED"))
+			System.out.printf("\n\"RED\" notFound at index %d\n",
+			vector.indexOf("RED"));
+		else
+			System.out.printf("\n\"RED\" notFound \n ");
+		
+		System.out.printf("\nSize: %d\nCapacity: %d\n",vector.size(),vector.capacity());
+	}
+
+	private void printVector(Vector<String> vector) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
