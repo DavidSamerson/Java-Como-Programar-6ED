533a58d62a462d50f8635c59a0723e957bac9c4e
DavidSamerson <samerson10@gmail.com>
2017-06-30 15:20:43 -0300

    reverse sort

---
+++ src/colecoes/Sort2.java
@@ -0,0 +1,29 @@
+package colecoes;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort2 {
+
+private static final String suits[] = {"Hearts", "Diamonds", "Clubs", "Spades"};
+	
+	public void printElements(){
+		
+		List<String> list = Arrays.asList(suits);
+		
+		System.out.printf("Unsorted Array Elements: \n%s\n",list);
+		
+		Collections.sort(list,Collections.reverseOrder());
+		
+		System.out.println("Sorted Array Elements: \n%s\n",list);
+	}
+
+	public static void main(String[] args) {
+		
+		Sort1 sort = new Sort1();
+		sort.printElements();
+		
+	}
+
+}
