1d0261bf77931e09766dd5f567bb0d0a157a0072
DavidSamerson <samerson10@gmail.com>
2017-06-30 16:21:13 -0300

    Classe TimeComparator

---
+++ src/colecoes/Time2.java
@@ -0,0 +1,20 @@
+package colecoes;
+
+public class Time2 {
+
+	public int getHour() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMinute() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getSecond() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
