9c3a1d1758dd94ed0a37d2e1a024fa14dcc14438
DavidSamerson <samerson10@gmail.com>
2017-06-30 15:12:08 -0300

    Coleções - sort - collections

--- src/colecoes/Sort1.java
+++ src/colecoes/Sort1.java
@@ -1,6 +1,23 @@
 package colecoes;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Sort1 {
+	
+	private static final String suits[] = {"Hearts", "Diamonds", "Clubs", "Spades"};
+	
+	public void printElements(){
+		
+		List<String> list = Arrays.asList(suits);
+		
+		System.out.printf("Unsorted Array Elements: \n%s\n",list);
+		
+		Collections.sort(list);
+		
+		System.out.println("Sorted Array Elements: \n%s\n");
+	}
 
 	public static void main(String[] args) {
 		
