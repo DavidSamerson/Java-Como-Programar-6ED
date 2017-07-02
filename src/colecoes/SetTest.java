1eb3fa50268d5dc9c497b106fd0741b4a7b467ed
DavidSamerson <samerson10@gmail.com>
2017-07-02 17:13:22 -0300

    Conjuntos - classe SET

--- src/colecoes/SetTest.java
+++ src/colecoes/SetTest.java
@@ -1,9 +1,31 @@
 package colecoes;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class SetTest {
+	
+	private static final String colors[] = {"red","white","blue","peach","gray","orange"};
 
 	public SetTest() {
 		
+		List<String> list = Arrays.asList(colors);
+		System.out.printf("Arrays: %s",list);
+		printNonDuplicates(list);
+	}
+
+	private void printNonDuplicates(Collection<String> collection) {
+		
+		Set<String> set = new HashSet<String>(collection);
+		System.out.println("\nNon Duplicates are: ");
+		
+		for (String string : set) {
+			System.out.printf("%s",string);
+		}
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
