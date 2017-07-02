856ec6be616308f937583ace8fb9d870ca67a413
DavidSamerson <samerson10@gmail.com>
2017-07-02 14:57:49 -0300

    Coleções - classe stack

--- src/colecoes/StackTest.java
+++ src/colecoes/StackTest.java
@@ -1,6 +1,9 @@
 package colecoes;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
+
+import EstruturasDeDados.ListasVinculadas.EmptyListException;
 
 public class StackTest {
 	
@@ -8,6 +11,39 @@ public class StackTest {
 		
 		Stack<Number> stack = new Stack<Number>();
 		
+		Long longNumber = 12L;
+		Integer intNumber = 34567;
+		Float floatNumber = 1.0F;
+		Double doubleNumber = 1234.5678;
+		
+		stack.push(longNumber);
+		printStack(stack);
+		stack.push(intNumber);
+		printStack(stack);
+		stack.push(floatNumber);
+		printStack(stack);
+		stack.push(doubleNumber);
+		printStack(stack);
+		
+		try {
+			
+			Number removeObject = null;
+			
+			while (true) {
+				
+				removeObject = stack.pop();//remove objeto do topo da pilha
+				System.out.printf("%s popped \n",removeObject);
+				printStack(stack);
+				
+			}
+		} catch (EmptyStackException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void printStack(Stack<Number> stack) {
+		// TODO Auto-generated method stub
 		
 	}
 
