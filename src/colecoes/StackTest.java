b739e811154f1e64c2783d6df85cd2d0df94dffc
DavidSamerson <samerson10@gmail.com>
2017-07-02 14:40:27 -0300

    stack - java.util

---
+++ src/colecoes/StackTest.java
@@ -0,0 +1,19 @@
+package colecoes;
+
+import java.util.Stack;
+
+public class StackTest {
+	
+	public StackTest() {
+		
+		Stack<Number> stack = new Stack<Number>();
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
