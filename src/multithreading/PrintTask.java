1b6e5957aafcda6976ebc531110a55dba37b9e4e
DavidSamerson <samerson10@gmail.com>
2017-07-03 15:31:44 -0300

    multithreading

--- src/multithreading/PrintTask.java
+++ src/multithreading/PrintTask.java
@@ -1,9 +1,26 @@
 package multithreading;
 
-public class PrintTask {
+import java.util.Random;
 
-	public PrintTask() {
-		// TODO Auto-generated constructor stub
+public class PrintTask implements Runnable{
+	
+	private int sleepTime;
+	private String threadName;
+	private static Random generator = new Random();
+
+	public PrintTask(String name)
+	{
+		threadName = name;
+		sleepTime = generator.nextInt(5000);
+	}
+	
+	public void run()
+	{
+		try {
+			System.out.printf("%s going to sleep for %d milliseconds.\n",threadName, sleepTime);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
