fb264b5e1c27c380fa1b6ee64eb885b1581eff0f
DavidSamerson <samerson10@gmail.com>
2017-07-04 15:43:25 -0300

    Thread

---
+++ src/multithreading/Consumer.java
@@ -0,0 +1,15 @@
+package multithreading;
+
+public class Consumer implements Runnable {
+
+	public Consumer() {
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
