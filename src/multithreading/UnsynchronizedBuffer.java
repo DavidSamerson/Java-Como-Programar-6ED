b1b2ecb98a20d24555fa6b3b07e0ff3b6cf0f2a5
DavidSamerson <samerson10@gmail.com>
2017-07-04 16:15:23 -0300

    Multithreading

---
+++ src/multithreading/UnsynchronizedBuffer.java
@@ -0,0 +1,20 @@
+package multithreading;
+
+public class UnsynchronizedBuffer implements Buffer {
+	
+	private int buffer = -1;
+
+	@Override
+	public void set(int value) {
+	
+		System.out.printf("Producer Writes\t%2d",value);
+		buffer = value;
+	}
+
+	@Override
+	public int get() {
+		System.out.printf("Consumer reads \t%2d",buffer);
+		return buffer;
+	}
+
+}
