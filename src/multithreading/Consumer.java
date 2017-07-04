9cc4a0287ed2045d8f69c41f24ce029b1c3b1155
DavidSamerson <samerson10@gmail.com>
2017-07-04 15:50:48 -0300

    Multithreading

--- src/multithreading/Consumer.java
+++ src/multithreading/Consumer.java
@@ -1,14 +1,32 @@
 package multithreading;
 
+import java.util.Random;
+
 public class Consumer implements Runnable {
 
-	public Consumer() {
-		
+	private static Random generator = new Random();
+	private Buffer sharedLocations;
+
+	public Consumer(Buffer shared) {
+		sharedLocations = shared;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+
+		int sum = 0;
+
+		for (int count = 1; count <= 10; count++) {
+			try {
+				Thread.sleep(generator.nextInt(3000));
+				sum += sharedLocations.get();
+				System.out.printf("\t\t\t%2d\n", sum);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		System.out.printf("\n%s %d.\n%s\n", "Consume read values totaling.", sum, "Terminating Consumer");
 
 	}
 
