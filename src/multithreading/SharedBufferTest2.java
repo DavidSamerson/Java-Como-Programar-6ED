0e2af939231843c0832aa05d6376cf26f010237b
DavidSamerson <samerson10@gmail.com>
2017-07-05 16:01:11 -0300

    Multithread

---
+++ src/multithreading/SharedBufferTest2.java
@@ -0,0 +1,26 @@
+package multithreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SharedBufferTest2 {
+
+	public static void main(String[] args) 
+	{	
+		ExecutorService application = Executors.newFixedThreadPool(2);
+		
+		Buffer sharedLocation = new SynchronizedBuffer();
+		
+		System.out.printf("%-40s%s\t\t%s\n%-40s%s\n\n","Operation","Buffer","Occupied", "----------","-------\t\t---------");
+		
+		try {
+			application.execute(new Producer(sharedLocation));
+			application.execute(new Consumer(sharedLocation));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		application.shutdown();
+	}
+
+}
