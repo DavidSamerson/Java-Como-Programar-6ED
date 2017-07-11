2f79569fd0b4abb295a7b6c5c973a28d10617780
DavidSamerson <samerson10@gmail.com>
2017-07-11 14:35:02 -0300

    classe teste

---
+++ src/redes/ReadServerFileTest.java
@@ -0,0 +1,14 @@
+package redes;
+
+import javax.swing.JFrame;
+
+public class ReadServerFileTest {
+
+	public static void main(String[] args) {
+		
+		ReadServerFile app = new ReadServerFile();
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
