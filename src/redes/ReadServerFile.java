1d9a1a89f6e8840fce2a79714d5dfa92c335d3fb
DavidSamerson <samerson10@gmail.com>
2017-07-11 13:56:41 -0300

    REDES - Lendo um arquivo de um servidor WEB

--- src/redes/ReadServerFile.java
+++ src/redes/ReadServerFile.java
@@ -1,7 +1,52 @@
 package redes;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 public class ReadServerFile extends JFrame {
+
+	private JTextField enterField;
+	private JEditorPane contentsArea;
+
+	public ReadServerFile() {
+		super("Simple WEB Brownser");
+
+		enterField = new JTextField("Enter file url here");
+		enterField.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getThePage(e.getActionCommand());
+			}
+		});
+		
+		add(enterField, BorderLayout.NORTH);
+		
+		contentsArea = new JEditorPane();
+		
+		contentsArea.setEditable(false);
+		contentsArea.addHyperlinkListener(new HyperlinkListener() {
+			
+			@Override
+			public void hyperlinkUpdate(HyperlinkEvent e) {
+				
+				if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+					getThePage (e.getURL().toString());
+				}
+			}
+		});
+		
+		add( new JScrollPane(contentsArea), BorderLayout.CENTER );
+		setSize(400, 300);
+		setVisible( true );
+	}
 
 }
