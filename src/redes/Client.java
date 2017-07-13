8ed2add5d1dd5412357cb6c581c710fa329de260
DavidSamerson <samerson10@gmail.com>
2017-07-13 14:41:56 -0300

    CLIENTE

--- src/redes/Client.java
+++ src/redes/Client.java
@@ -4,8 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.EOFException;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 
 import javax.swing.JFrame;
@@ -14,7 +17,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Client extends JFrame {
-	
+
 	private JTextField enterField;
 	private JTextArea displayArea;
 	private ObjectOutputStream output;
@@ -23,31 +26,62 @@ public class Client extends JFrame {
 	private String chatServer;
 	private Socket client;
 
-	//CONFIGURAÇÃO DA INTERFACE GRAFICA
+	// CONFIGURAÇÃO DA INTERFACE GRAFICA
 	public Client(String host) throws HeadlessException {
-		
+
 		super("Client");
-		
+
 		chatServer = host;
-		
+
 		enterField = new JTextField();
 		enterField.setEditable(false);
 		enterField.addActionListener(new ActionListener() {
-			
+
 			@Override
-			public void actionPerformed(ActionEvent event) 
-			{
+			public void actionPerformed(ActionEvent event) {
 				sendData(event.getActionCommand());
 				enterField.setText("");
 			}
 		});
-		
+
 		add(enterField, BorderLayout.NORTH);
-		
+
 		displayArea = new JTextArea();
-		add(new JScrollPane( displayArea ), BorderLayout.CENTER);
-		
-		setSize(300,150);
+		add(new JScrollPane(displayArea), BorderLayout.CENTER);
+
+		setSize(300, 150);
 		setVisible(true);
+	}
+
+	private void runClient(){
+		
+		try {
+			
+			connectToServer();
+			getStreams();
+			processConnection();
+			
+		} catch (EOFException e) {
+			displayMessage("\nClient terminated")
+		} catch (IOException e){
+			e.printStackTrace();
+		} finally{
+			closeConnection();
+		}
+	}
+
+	private void connectToServer() throws IOException {
+
+		displayMessage("Attempting Connection");
+
+		client = new Socket( InetAddress.getByName(chatServer), 12345 );
+		
+		displayMessage("Connected to: "+ client.getInetAddress().getHostName());
+
+	}
+
+	private void displayMessage(String string) {
+		// TODO Auto-generated method stub
+
 	}
 }
