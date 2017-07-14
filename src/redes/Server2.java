46e09de591659e19df8270a35b1cf700a9441b11
DavidSamerson <samerson10@gmail.com>
2017-07-14 16:14:37 -0300

    Servidor 2

--- src/redes/Server2.java
+++ src/redes/Server2.java
@@ -1,6 +1,8 @@
 package redes;
 
 import java.awt.BorderLayout;
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
@@ -30,8 +32,29 @@ public class Server2 extends JFrame {
 			e.printStackTrace();
 			System.exit(1);
 		}
+	}
+	
+	public void waitForPackets(){
 		
+		while (true) {
+			try {
+				
+				byte data[] = new byte[100];
+				DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+				socket.receive(receivePacket);
+				
+				displayMessage("\nPacket Received ...");
+				
+				sendPacketToClient(receivePacket);
+				
+			} catch (IOException e) {
+				displayMessage(e.toString() + "\n");
+				e.printStackTrace();
+			}
+		}
 		
 	}
+	
+	
 
 }
